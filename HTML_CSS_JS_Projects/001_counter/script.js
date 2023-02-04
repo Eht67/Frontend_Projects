ecbb2e3a13f72c6f72e1977fe9c42084fb784a79
Ehtsham_Hussain <logictechie@gmail.com>
2023-02-04 12:45:35 +0500

    finished counter project

---
+++ HTML_CSS_JS_Projects/001_counter/script.js
@@ -0,0 +1,19 @@
+let numb = 0;
+let numb_display = document.getElementsByTagName('p')[0];
+numb_display.innerHTML = numb;
+
+let reset_btn = document.getElementById('reset-btn');
+let count_btn = document.getElementById('count-btn');
+
+const reset_func = () => {
+    numb = 0;
+    numb_display.innerHTML = numb;
+}
+
+const count_func = () => {
+    numb++;
+    numb_display.innerHTML = numb;
+}
+
+reset_btn.addEventListener('click', reset_func);
+count_btn.addEventListener('click', count_func);
