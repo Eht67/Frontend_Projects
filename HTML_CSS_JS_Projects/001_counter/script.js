7d053941f31a2f7873a0c254b5d14cee50637bd6
Ehtsham_Hussain <logictechie@gmail.com>
2023-02-04 18:38:08 +0500

    added reverse function

--- HTML_CSS_JS_Projects/001_counter/script.js
+++ HTML_CSS_JS_Projects/001_counter/script.js
@@ -4,6 +4,7 @@ numb_display.innerHTML = numb;
 
 let reset_btn = document.getElementById('reset-btn');
 let count_btn = document.getElementById('count-btn');
+let reverse_btn = document.getElementById('reverse-btn');
 
 const reset_func = () => {
     numb = 0;
@@ -15,5 +16,11 @@ const count_func = () => {
     numb_display.innerHTML = numb;
 }
 
+const reverse_func = () => {
+    numb--;
+    numb_display.innerHTML = numb;
+}
+
 reset_btn.addEventListener('click', reset_func);
 count_btn.addEventListener('click', count_func);
+reverse_btn.addEventListener('click', reverse_func);
