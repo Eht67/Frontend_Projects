7aea9defe35594f13eb4e3fdf9c1eed11f0d874c
Ehtsham_Hussain <logictechie@gmail.com>
2023-02-09 13:57:35 +0500

    fixed audio loading issue and empty entry issue

--- HTML_CSS_JS_Projects/003_digital_clock_with_alarm/script.js
+++ HTML_CSS_JS_Projects/003_digital_clock_with_alarm/script.js
@@ -2,6 +2,11 @@ let hours_display = document.getElementsByTagName('p')[0];
 let minutes_display = document.getElementsByTagName('p')[1];
 let seconds_display = document.getElementsByTagName('p')[2];
 
+let audio;
+setTimeout(() => {
+    audio = new Audio('alarm.mp3');
+}, 5000);
+
 let hours;
 let minutes;
 let seconds;
@@ -29,8 +34,8 @@ const set_alarm_func = () => {
     alarm_hours = prompt('Enter hours of alarm in 24 hours format.');
     alarm_minutes = prompt('Enter minutes of alarm.');
 
-    alarm_hours = Number.parseInt(alarm_hours);
-    alarm_minutes = Number.parseInt(alarm_minutes);
+    alarm_hours = Number.parseInt(alarm_hours) || 0;
+    alarm_minutes = Number.parseInt(alarm_minutes) || 0;
 
     let display_alarm_hours = alarm_hours < 10 ? `0${alarm_hours}` : alarm_hours;
     let display_alarm_minutes = alarm_minutes < 10 ? `0${alarm_minutes}` : alarm_minutes;
@@ -44,7 +49,6 @@ let interval;
 
 const ring_alarm_func = () => {
     if (alarm_hours == hours && alarm_minutes == minutes) {
-        let audio = new Audio('alarm.mp3');
         audio.play();
         clearInterval(interval)
     }
