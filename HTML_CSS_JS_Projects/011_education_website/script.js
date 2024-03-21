3244a78b5051c3594404c5eab5fe4f6420794b67
Eht67 <logictechie@gmail.com>
2024-03-21 15:54:56 +0500

    added mobile menu in education website

--- HTML_CSS_JS_Projects/011_education_website/script.js
+++ HTML_CSS_JS_Projects/011_education_website/script.js
@@ -1,11 +1,34 @@
+// getting header element
 let header = document.querySelector('.header-area');
 
+// making header sticky on scroll past a specific point
 document.onscroll = () => {
 if (window.scrollY >= 900){
     header.classList.add('header-area-scrolled');
 }
-
 else{
     header.classList.remove('header-area-scrolled');
 }
 }
+
+let mobileMenuIcon = document.querySelector('#mobile-menu-icon');
+let mobileMenuContent = document.querySelector('#mobile-menu-content');
+let mobileMenuExpanded = false;
+
+
+mobileMenuIcon.addEventListener('click', ()=>{
+    if (!mobileMenuExpanded){
+        mobileMenuContent.style.height = '43rem';
+        mobileMenuExpanded = true;
+    }
+
+    else if (mobileMenuExpanded){
+        mobileMenuContent.style.height = 0;
+        mobileMenuExpanded = false;
+    }
+})
+
+mobileMenuContent.addEventListener('click', ()=>{
+    mobileMenuContent.style.height = 0;
+    mobileMenuExpanded = false;
+})
