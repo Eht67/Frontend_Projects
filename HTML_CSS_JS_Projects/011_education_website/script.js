66f3e6e5b7c0fbd2d10824625645933eda59f564
Eht67 <logictechie@gmail.com>
2024-03-21 16:37:26 +0500

    chnage icon to close on opening mobile menu

--- HTML_CSS_JS_Projects/011_education_website/script.js
+++ HTML_CSS_JS_Projects/011_education_website/script.js
@@ -3,12 +3,12 @@ let header = document.querySelector('.header-area');
 
 // making header sticky on scroll past a specific point
 document.onscroll = () => {
-if (window.scrollY >= 900){
-    header.classList.add('header-area-scrolled');
-}
-else{
-    header.classList.remove('header-area-scrolled');
-}
+    if (window.scrollY >= 900) {
+        header.classList.add('header-area-scrolled');
+    }
+    else {
+        header.classList.remove('header-area-scrolled');
+    }
 }
 
 let mobileMenuIcon = document.querySelector('#mobile-menu-icon');
@@ -16,19 +16,25 @@ let mobileMenuContent = document.querySelector('#mobile-menu-content');
 let mobileMenuExpanded = false;
 
 
-mobileMenuIcon.addEventListener('click', ()=>{
-    if (!mobileMenuExpanded){
+mobileMenuIcon.addEventListener('click', () => {
+    if (!mobileMenuExpanded) {
         mobileMenuContent.style.height = '43rem';
+        mobileMenuIcon.classList.remove("fa-bars");
+        mobileMenuIcon.classList.add("fa-close");
         mobileMenuExpanded = true;
     }
 
-    else if (mobileMenuExpanded){
+    else if (mobileMenuExpanded) {
         mobileMenuContent.style.height = 0;
+        mobileMenuIcon.classList.remove("fa-close");
+        mobileMenuIcon.classList.add("fa-bars");
         mobileMenuExpanded = false;
     }
 })
 
-mobileMenuContent.addEventListener('click', ()=>{
+mobileMenuContent.addEventListener('click', () => {
     mobileMenuContent.style.height = 0;
+    mobileMenuIcon.classList.remove("fa-close");
+    mobileMenuIcon.classList.add("fa-bars");
     mobileMenuExpanded = false;
 })
