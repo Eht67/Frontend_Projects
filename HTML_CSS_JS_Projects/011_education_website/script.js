e2fb84726a3fca5d76ab432e93b96feaf9f9c54b
Eht67 <logictechie@gmail.com>
2024-07-10 11:10:11 +0500

    add logic to carousels and make them responsive

--- HTML_CSS_JS_Projects/011_education_website/script.js
+++ HTML_CSS_JS_Projects/011_education_website/script.js
@@ -40,3 +40,58 @@ mobileMenuContent.addEventListener('click', () => {
     mobileMenuIcon.classList.add("fa-bars");
     mobileMenuExpanded = false;
 })
+
+let carousels = document.querySelectorAll('.carousel');
+
+for (let i = 0; i < carousels.length; i++) {
+    let carousel = carousels[i];
+    let left_btn = carousel.querySelectorAll('.left-button')[0];
+    let right_btn = carousel.querySelectorAll('.right-button')[0];
+    let carousel_inner = carousel.querySelectorAll('.carousel-inner')[0];
+    let carousel_item_width = carousel_inner.querySelectorAll('.carousel-item')[0].offsetWidth;
+
+    carousel.addEventListener('mouseover', () => {
+        document.addEventListener('keydown', handleKeyboardKeys);
+    });
+
+    carousel.addEventListener('mouseout', () => {
+        document.removeEventListener('keydown', handleKeyboardKeys);
+    });
+
+    function handleKeyboardKeys(event) {
+        const keyCode = event.keyCode;
+        if (keyCode === 37) {
+            carousel_inner.scrollLeft = carousel_inner.scrollLeft - carousel_item_width;
+        }
+        else if (keyCode === 39) {
+            carousel_inner.scrollLeft = carousel_inner.scrollLeft + carousel_item_width;
+        }
+    }
+
+    left_btn.addEventListener('click', () => {
+        carousel_inner.scrollLeft = carousel_inner.scrollLeft - carousel_item_width;
+    })
+
+    right_btn.addEventListener('click', () => {
+        carousel_inner.scrollLeft = carousel_inner.scrollLeft + carousel_item_width;
+    })
+
+    if (carousel_inner.scrollLeft === 0) {
+        left_btn.classList.add('disabled');
+    }
+
+    carousel_inner.addEventListener('scroll', () => {
+        if (carousel_inner.scrollLeft === 0) {
+            left_btn.classList.add('disabled');
+        }
+
+        if (carousel_inner.scrollLeft > 0 && carousel_inner.scrollLeft < (carousel_inner.scrollWidth - carousel_inner.offsetWidth)) {
+            left_btn.classList.remove('disabled');
+            right_btn.classList.remove('disabled');
+        }
+
+        if (carousel_inner.scrollLeft === carousel_inner.scrollWidth - carousel_inner.offsetWidth) {
+            right_btn.classList.add('disabled');
+        }
+    })
+}
